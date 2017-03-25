ec4ac09cd7bd8084f4b5423cba9039abf396a07c
Eliott <developpsoft@gmail.com>
2017-03-25 16:44:37 +0000

    Script d'install

---
+++ run.sh
@@ -0,0 +1,9 @@
+# Mise à jour
+sudo apt update
+sudo apt --yes upgrade
+sudo apt --yes dist-upgrade
+
+# Dépendances
+sudo apt install git python3 python3-dev python-dev python3-pip python3-pyaudio sox
+
+sudo pip3 install SpeechRecognition
