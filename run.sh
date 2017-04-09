4a7b30c47f8f072fbdc2b46500c10dafa3aae502
Eliott <developpsoft@gmail.com>
2017-04-09 15:36:08 +0000

    Modification pyaudio

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@ sudo apt --yes upgrade
 sudo apt --yes dist-upgrade
 
 # Dépendances
-sudo apt install git python3 python3-dev python-dev python3-pip python3-pyaudio sox build-essential
+sudo apt install git python3 python3-dev python-dev python3-pip sox build-essential
 
 sudo pip3 install SpeechRecognition
+sudo pip3 install pyaudio
 
 git clone https://github.com/MiloProject/snowboy
 
