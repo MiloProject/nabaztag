d18da1dfd6a1b7cc407c1eba944381004db939ec
Eliott T <developpsoft@gmail.com>
2017-04-09 14:58:21 +0000

    Ajoute SLEEP_TIME

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@
 # Snowboy
 MODEL = "../snowboy.umdl"
 SENSIBILITE = 0.5 # entre 0 et 1!
+SLEEP_TIME = 0.03 # Temps entre chaque recherche de mot clef
 
 # Chemin vers les fichiers audio
 AUDIO = "../data/audio/"
