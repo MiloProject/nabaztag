58b95622eff056ac0ceee65b7d6119b697d36920
Eliott <developpsoft@gmail.com>
2017-04-09 15:33:26 +0000

    Update installation

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ sudo apt --yes upgrade
 sudo apt --yes dist-upgrade
 
 # Dépendances
-sudo apt install git python3 python3-dev python-dev python3-pip python3-pyaudio sox
+sudo apt install git python3 python3-dev python-dev python3-pip python3-pyaudio sox build-essential
 
 sudo pip3 install SpeechRecognition
+
+git clone https://github.com/MiloProject/snowboy
+
+cd snowboy/swig/python
+make
