a729807e21ab703c59936360b0db26c404f28cd6
Eliott <developpsoft@gmail.com>
2017-04-09 15:37:52 +0000

    Installation auto

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sudo apt --yes upgrade
 sudo apt --yes dist-upgrade
 
 # Dépendances
-sudo apt install git python3 python3-dev python-dev python3-pip sox build-essential
+sudo apt install -y git python3 python3-dev python-dev python3-pip sox build-essential
 
 sudo pip3 install SpeechRecognition
 sudo pip3 install pyaudio
