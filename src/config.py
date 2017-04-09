d19fd256258642bd9a4a26390356cf6ff8b47984
Eliott T <developpsoft@gmail.com>
2017-04-09 14:50:08 +0000

    Ajoute SENSIBILITE

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 # Configure le projet
 
-# Chemin vers le model snowboy
+# Snowboy
 MODEL = "../snowboy.umdl"
+SENSIBILITE = 0.5 # entre 0 et 1!
 
 # Chemin vers les fichiers audio
 AUDIO = "../data/audio/"
