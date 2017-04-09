ec120efae9dc75bd61b19281c215a3aa9f67eb0e
Eliott <developpsoft@gmail.com>
2017-04-09 16:07:07 +0200

    Ajoute espeak

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sudo apt --yes upgrade
 sudo apt --yes dist-upgrade
 
 # Dépendances
-sudo apt install -y git python3 python3-dev python-dev python3-pip sox build-essential portaudio19-dev flac
+sudo apt install -y git python3 python3-dev python-dev python3-pip sox build-essential portaudio19-dev flac espeak
 
 sudo pip3 install SpeechRecognition
 sudo pip3 install pyaudio
