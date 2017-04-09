98992efb793a69c534dd9ec28d06a60332d03539
Eliott T <developpsoft@gmail.com>
2017-04-09 15:17:39 +0000

    Ajoute AUDIO_OK, AUDIO_START

--- src/config.py
+++ src/config.py
@@ -7,6 +7,8 @@ SLEEP_TIME = 0.03 # Temps entre chaque recherche de mot clef
 
 # Chemin vers les fichiers audio
 AUDIO = "../data/audio/"
+AUDIO_OK = ""
+AUDIO_START = ""
 
 # Hote vers qui tester la connexion, site d'Eliott
 TEST_HOST = ("developpsoft.github.io", 80)
