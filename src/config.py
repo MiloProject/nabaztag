e779bea689180692a3e9e97fe5dd2bdbcd278074
Eliott T <developpsoft@gmail.com>
2017-04-09 15:05:34 +0000

    Ajoute TEST_HOST

--- src/config.py
+++ src/config.py
@@ -7,3 +7,6 @@ SLEEP_TIME = 0.03 # Temps entre chaque recherche de mot clef
 
 # Chemin vers les fichiers audio
 AUDIO = "../data/audio/"
+
+# Hote vers qui tester la connexion, site d'Eliott
+TEST_HOST = ("developpsoft.github.io", 80)
