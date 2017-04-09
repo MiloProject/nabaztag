0ef438c38af8f84746733965348919903f69faa9
Eliott T <developpsoft@gmail.com>
2017-04-09 15:12:43 +0000

    Test de connexion

--- src/wifi.py
+++ src/wifi.py
@@ -2,3 +2,25 @@
 
 import socket as sock
 
+def teste(to_conn):
+    """
+    Teste la connexion Internet
+    """
+    result = None
+    
+    s = sock.socket()
+    
+    try:
+        s.connect((to_conn))
+        s.send("GET /") # Récupère la page principale
+    except:
+        # Il y un erreur donc pas de connexion
+        result = False
+    else:
+        # si tout vas bien
+        result = True
+    finally:
+        # On oublie pas de fermer la connexion
+        s.close()
+        
+    return result
