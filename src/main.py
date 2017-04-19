c616047a9d84aa3bbf0aefd2599ec7c8f25180c3
Eliott T <developpsoft@gmail.com>
2017-04-19 10:08:25 +0200

    Modification appels à config

--- src/main.py
+++ src/main.py
@@ -3,17 +3,17 @@ from snowboy_detect import decoder
 
 from processor import dit, joue
 from recon import reco_vocale
+from config import AUDIO_OK, SENSIBILITE, MODEL, TEST_HOST, SLEEP_TIME
 
 # Pour les interruptions
 import signal
 
 import wifi
-import config
 
-joue(config.AUDIO_START)
+joue(AUDIO_START)
 
 dit("Je vais vérifier la connexion")
-if not wifi.teste(config.TEST_HOST):
+if not wifi.teste(TEST_HOST):
     # On n'est pas connecté
     dit("Je ne suis pas connecté")
     dit("Démarrage du réseau wifi")
@@ -42,13 +42,13 @@ else:
     signal.signal(signal.SIGTERM, signal_handler)
     
     # Détecte le mot clef (Milo)
-    detecteur = snowboydecoder.HotwordDetector(config.MODEL, sensitivity=config.SENSIBILITE)
+    detecteur = snowboydecoder.HotwordDetector(MODEL, sensitivity=SENSIBILITE)
     print('[MAIN] En écoute...')
     
     # Dès qu'il y un mot clef, on démarre la reconnaissance vocale
     detecteur.start(detected_callback=reco_vocale, # Action dès que l'on capte un mot clef
                    interrupt_check=interrupt_callback, # Check si on s'est arrêté
-                   sleep_time=config.SLEEP_TIME)
+                   sleep_time=SLEEP_TIME)
     
     # Si on arrive ici, le detecteur s'est arrêté
     detecteur.terminate()
