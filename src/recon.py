496d70885ee53b95b8617181a6f994a21f2686f1
Eliott T <developpsoft@gmail.com>
2017-04-19 10:04:18 +0200

    Ajout reconnaissance vocale

--- src/recon.py
+++ src/recon.py
@@ -1,1 +1,33 @@
 # Le reconnaissance vocale
+
+import speech_recognition as sr
+
+from processor import process
+
+recon = sr.Recognizer()
+micro = sr.Microphone()
+
+def calibre():
+  print("[RECO] Calibration...")
+  with micro as source: recon.adjust_for_ambient_noise(source)
+    print("[RECO] Calibration : {}".format(recon.energy_threshold))
+  
+# Prend n'importe quel argument(s)
+def reco_vocale(*args, **kwargs):
+  print("[RECO] En écoute...")
+  with micro as source: audio = recon.listen(source)
+  print("[RECO] Entendu quelque chose...")
+  try:
+    phrase = recon.recognize_google(audio, language="fr-FR")
+    print("[RECO] Phrase : {}".format(phrase))
+  except sr.UnknownValueError:
+    # TODO: son erreur
+    print("[RECO] Valeur inconue... :(")
+  except sr.RequestError as e:
+    # TODO: son erreur
+    print("[RECO] Je ne peut pas contacter Google :(")
+    print("[RECO] Erreur: {}".format(e))
+  else: # Si tout vas bien
+    #TODO: son ok
+    process(phrase)
+    
