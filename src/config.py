60c2c50f18a8deff6cd49826878882ec85d4f295
Eliott T <developpsoft@gmail.com>
2017-04-26 10:14:47 +0200

    Update chemins

--- src/config.py
+++ src/config.py
@@ -7,9 +7,9 @@ SLEEP_TIME = 0.03 # Temps entre chaque recherche de mot clef
 
 # Chemin vers les fichiers audio
 AUDIO = "../data/audio/"
-AUDIO_OK = ""
-AUDIO_START = ""
-AUDIO_ERR = ""
+AUDIO_OK = AUDIO + "utils/ok.wav"
+AUDIO_START = AUDIO + "utils/start.wav"
+AUDIO_ERR = AUDIO + "utils/err.wav"
 
 AUDIO_CHUNK = 1024 # pour pyaudio
 
