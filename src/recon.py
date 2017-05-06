ec8224f47a06688729dd11ea5ab30898832626b0
Eliott <developpsoft@gmail.com>
2017-05-06 17:29:27 +0200

    Fix recon.py

--- src/recon.py
+++ src/recon.py
@@ -9,26 +9,27 @@ recon = sr.Recognizer()
 micro = sr.Microphone()
 
 def calibre():
-  print("[RECO] Calibration...")
-  with micro as source: recon.adjust_for_ambient_noise(source)
-    print("[RECO] Calibration : {}".format(recon.energy_threshold))
+	print("[RECO] Calibration...")
+	with micro as source: recon.adjust_for_ambient_noise(source)
+	print("[RECO] Calibration : {}".format(recon.energy_threshold))
   
 # Prend n'importe quel argument(s)
 def reco_vocale(*args, **kwargs):
-  print("[RECO] En écoute...")
-  with micro as source: audio = recon.listen(source)
-  print("[RECO] Entendu quelque chose...")
-  try:
-    phrase = recon.recognize_google(audio, language="fr-FR")
-    print("[RECO] Phrase : {}".format(phrase))
-  except sr.UnknownValueError:
-    joue(AUDIO_ERR)
-    print("[RECO] Valeur inconue... :(")
-  except sr.RequestError as e:
-    joue(AUDIO_ERR)
-    print("[RECO] Je ne peux pas contacter Google :(")
-    print("[RECO] Erreur: {}".format(e))
-  else: # Si tout vas bien
-    joue(AUDIO_OK)
-    process(phrase)
+	print("[RECO] En écoute...")
+	with micro as source: audio = recon.listen(source)
+	print("[RECO] Entendu quelque chose...")
+
+	try:
+		phrase = recon.recognize_google(audio, language="fr-FR")
+		print("[RECO] Phrase : {}".format(phrase))
+	except sr.UnknownValueError:
+		joue(AUDIO_ERR)
+		print("[RECO] Valeur inconue... :(")
+	except sr.RequestError as e:
+		joue(AUDIO_ERR)
+		print("[RECO] Je ne peux pas contacter Google :(")
+		print("[RECO] Erreur: {}".format(e))
+	else: # Si tout vas bien
+		joue(AUDIO_OK)
+		process(phrase)
     
